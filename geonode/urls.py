a79717899066098f45932dc43fccdf844c99752b
Anton Gritsay <anton@angri.ru>
2011-11-01 12:01:19 +0100

    use the same url for new and edit observation

--- geonode/urls.py
+++ geonode/urls.py
@@ -4,5 +4,5 @@ urlpatterns = patterns('geonode.observations.views',
    (r'^obsform/edit/(?P<observation_id>\d+)/summary_id/(?P<summary_id>\d+)$',
         'edit'),
    (r'^obsform/new/summary_id/(?P<summary_id>\d+)$',
-        'new'),
+        'edit', {'observation_id': None}),
 )
